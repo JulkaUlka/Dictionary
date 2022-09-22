26b7337855be180011107f8e8422521c80f4bb2e
JulkaUlka <y.glushko@outlook.com>
2022-09-22 16:48:12 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Hello</header>
+      <button className="btn btn-primary">Start</button>
+    </div>
+  );
+}
+
+export default App;
