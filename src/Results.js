8e9c570ee3bcbcfc1ae662b204c5fe5cea94ad5b
JulkaUlka <y.glushko@outlook.com>
2022-09-29 12:24:35 +0300

    added phonetics block

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Meaning from "./Meaning.js";
+import Phonetic from "./Phonetic.js";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
         <h3>{props.results.word}</h3>
+        {props.results.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetic phonetic={phonetic} />
+            </div>
+          );
+        })}
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
