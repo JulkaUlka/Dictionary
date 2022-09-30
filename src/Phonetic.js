54f64b65f4b535e429b79d76a103abec1f676043
JulkaUlka <y.glushko@outlook.com>
2022-09-30 15:43:09 +0300

    fixed Phonetic component

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,15 +2,19 @@ import React from "react";
 import ReactAudioPlayer from "react-audio-player";
 
 export default function Phonetic(props) {
-  return (
-    <div className="Phonetic">
-      <div>{props.phonetic.text}</div>
+  if (props.phonetic.audio) {
+    return (
+      <div className="Phonetic">
+        <div>{props.phonetic.text}</div>
 
-      <ReactAudioPlayer
-        className="phonetic-audio mt-1"
-        src={props.phonetic.audio}
-        controls
-      />
-    </div>
-  );
+        <ReactAudioPlayer
+          className="phonetic-audio mt-1"
+          src={props.phonetic.audio}
+          controls
+        />
+      </div>
+    );
+  } else {
+    return <div>{props.phonetic.text}</div>;
+  }
 }
